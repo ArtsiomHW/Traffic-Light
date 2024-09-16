5f521a9a34133123f2386152bca6426bee4ea464
Artsiom <artsiom.hurynovich@gmail.com>
2024-09-16 15:51:50 +0200

    Initial Commit

---
+++ Lesson 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 2
+//
+//  Created by Artem H on 16.09.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
